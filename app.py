20a270e5309053fbb33b533c63fcdad60b012e2e
gerd113 <gerd112@hotmail.com>
2020-11-13 17:17:10 +0000

    Created flask app and linked to database.

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Polygon FA"
+@app.route("/get_rota")
+def get_rota():
+    rota = mongo.db.client_coverage.find()
+    return render_template("tasks.html", rota=rota)
 
 
 if __name__ == "__main__":
